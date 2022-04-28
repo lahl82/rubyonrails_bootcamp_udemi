7112dafa2e871b008c20d2c88b2de250490affb5
Ubuntu <ubuntu@ip-172-31-9-124.ec2.internal>
2022-04-28 19:12:24 +0000

    actionText configured

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,6 +1,6 @@
 class Course < ApplicationRecord
   validates :title, presence: true
-  validates :description, presence: true, length: {:minimum => 5}
+  #validates :description, presence: true, length: {:minimum => 5}
 
   def to_s
     title
